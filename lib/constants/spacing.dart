24b7a33dbec653458ea2c867b2164e6018e576de
LioQing <s2010157@gmail.com>
2024-08-04 21:28:18 +0800

    Add drag dropdown

--- lib/constants/spacing.dart
+++ lib/constants/spacing.dart
@@ -1,7 +1,9 @@
 class SpacingConsts {
   static const xs = 4.0;
   static const s = 8.0;
+  static const ms = 12.0;
   static const m = 16.0;
+  static const ml = 24.0;
   static const l = 32.0;
   static const xl = 64.0;
 }
